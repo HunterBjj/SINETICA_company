5afd6bba961c92e106d2e6a01cb080ee527f0e85
Урманов Артем Александрович <urmanov_aa@niipp.ru>
2024-10-18 09:46:55 +0700

    add deploy

---
+++ build/Qt_6_7_2_mingw_64-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_QT_TEST_FILE C:/Qt/QT_TEST/build/Qt_6_7_2_mingw_64-Debug/QT_TEST.exe)
+set(__QT_DEPLOY_TARGET_QT_TEST_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_QT_TEST_RUNTIME_DLLS D:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
